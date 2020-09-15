c8c53e9f33418324e98d92b05f90b2cddbe10b14
heyimnowi <lopeznoeliab@gmail.com>
2020-09-15 19:06:24 -0300

    Basic racer game

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+    // Start is called before the first frame update
+
+    public PlayerMovement movement;
+    public GameManager gameManager;
+    void OnCollisionEnter(Collision collisionInfo) {
+
+        if (collisionInfo.collider.tag == "Obstacle") {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
